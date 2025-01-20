c7f040c4f6f2edaba1b3bb542afee574357532df
WonGi-Kim <kwg970310@gmail.com>
2025-01-20 18:53:25 +0900

    add : Employee 구성

---
+++ src/main/java/com/example/dostep/domain/employee/model/Employee.java
@@ -0,0 +1,25 @@
+package com.example.dostep.domain.employee.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Map;
+
+@Getter
+@Document(collection = "employees")
+public class Employee {
+    @Id
+    private String id;
+    private PersonalInfo personalInfo;
+    private Account account;
+    private Map<String, Integer> expList;
+
+    @Builder
+    public Employee(PersonalInfo personalInfo, Account account, Map<String, Integer> expList) {
+        this.personalInfo = personalInfo;
+        this.account = account;
+        this.expList = expList;
+    }
+}
