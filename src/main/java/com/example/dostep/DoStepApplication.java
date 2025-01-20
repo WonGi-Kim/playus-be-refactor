2d1f416c4f644a1779eb0db313eb4d38dd437a20
WonGi-Kim <kwg970310@gmail.com>
2025-01-20 16:33:13 +0900

    add : first commit

---
+++ src/main/java/com/example/dostep/DoStepApplication.java
@@ -0,0 +1,13 @@
+package com.example.dostep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoStepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoStepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dostep/DoStepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dostep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoStepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
