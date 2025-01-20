6962fe9776f0a4f8c33f267c36a9f081483e3c2f
WonGi-Kim <kwg970310@gmail.com>
2025-01-20 20:48:05 +0900

    feat : JPA 없이 데이터 저장

--- src/main/java/com/example/dostep/domain/employee/EmployeeService.java
+++ src/main/java/com/example/dostep/domain/employee/EmployeeService.java
@@ -3,6 +3,7 @@ package com.example.dostep.domain.employee;
 import com.example.dostep.domain.employee.model.Employee;
 import com.example.dostep.domain.sheet.GoogleSheetHelper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EmployeeService {
 
-    private final EmployeeRepository employeeRepository;
+    private final MongoTemplate mongoTemplate;
     private static final GoogleSheetHelper googleSheetsHelper = new GoogleSheetHelper();
 
     public void syncEmployeeData(String spreadSheetId, String employeeRANGE) {
@@ -20,18 +21,21 @@ public class EmployeeService {
 
             List<Employee> newEmployees = EmployeeConvert.convertToUsers(sheetData);
 
-            // 기존 데이터 조회
-            List<Employee> existingEmployees = employeeRepository.findAll();
+            // 기존 데이터 조회 (MongoDB에서 employeeId 기준으로 가져오기)
+            List<Long> existingEmployeeIds = mongoTemplate
+                    .findAll(Employee.class)
+                    .stream()
+                    .map(employee -> employee.getPersonalInfo().getEmployeeId())
+                    .toList();
 
             // 중복되지 않는 새로운 데이터 필터링
             List<Employee> employeesToSave = newEmployees.stream()
-                    .filter(newEmployee -> existingEmployees.stream()
-                            .noneMatch(existingEmployee -> existingEmployee.getPersonalInfo().getEmployeeId().equals(newEmployee.getPersonalInfo().getEmployeeId())))
+                    .filter(newEmployee -> !existingEmployeeIds.contains(newEmployee.getPersonalInfo().getEmployeeId()))
                     .toList();
 
             // 새로운 데이터 저장
             if (!employeesToSave.isEmpty()) {
-                employeeRepository.saveAll(employeesToSave);
+                mongoTemplate.insertAll(employeesToSave);
             } else {
                 System.out.println("No new data to save");
             }
@@ -39,7 +43,6 @@ public class EmployeeService {
         } catch (Exception e) {
             throw new RuntimeException("Error while syncing Google Sheets to MongoDB: " + e.getMessage());
         }
-
-
     }
 }
+
